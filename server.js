2da8638789cc336e81f63f94d73f4f9632d104f3
Alok <alokd5905@gmail.com>
2022-12-11 12:32:43 +0530

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const app=express();
 const path=require("path");
+const cors=require('cors');
 
 const PORT=process.env.PORT || 3000;
 app.use(express.static('public'));
@@ -8,6 +9,12 @@ app.use(express.json())
 
 const connectDB=require('./config/db');
 connectDB();
+
+const corsOptions={
+    origin:process.env.ALLOWED_CLIENTS.split(',')
+    // ['http://localhost:3000','http://localhost:5000','http://localhost:3300']
+}
+app.use(cors(corsOptions))
 
 //template engine
 app.set('views',path.join(__dirname,'/views'));
